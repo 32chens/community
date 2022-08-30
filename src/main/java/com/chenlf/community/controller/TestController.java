0cd56e8af37c36c514be3bf67d8e277d1cb760cf
chenlf <647854697@qq.com>
2022-08-30 22:52:21 +0800

    了解Cookie和Session的使用

---
+++ src/main/java/com/chenlf/community/controller/TestController.java
@@ -0,0 +1,51 @@
+package com.chenlf.community.controller;
+
+
+import com.sun.deploy.net.HttpResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.net.HttpCookie;
+
+/**
+ * 
+ * @author ChenLF
+ * @date 2022/08/30 22:33
+ **/
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/setCookie")
+    public String setCookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("code", "setcookie");
+        cookie.setPath("/community/test");
+        cookie.setMaxAge(60 * 10);
+        response.addCookie(cookie);
+        return "set cookie";
+    }
+
+    @RequestMapping("/getCookie")
+    public String getCookie(@CookieValue("code") String cookie){
+        System.out.println(cookie);
+        return "get cookie";
+    }
+
+    @RequestMapping("/setSession")
+    public String setSession(HttpSession session){
+        session.setAttribute("name","chen");
+        return "set session";
+    }
+
+    @RequestMapping("/getSession")
+    public String getSession(HttpSession session){
+
+        return (String) session.getAttribute("name");
+    }
+}
---
+++ src/test/java/com/chenlf/community/Test.java
@@ -0,0 +1,12 @@
+package com.chenlf.community;
+
+import org.springframework.boot.web.server.Cookie;
+
+/**
+ * 
+ * @author ChenLF
+ * @date 2022/08/30 22:31
+ **/
+ 
+public class Test {
+}
