585829ee721805a452cbff90de2f4f1f17204f91
chenlf <647854697@qq.com>
2022-09-20 23:52:51 +0800

    帖子详情

--- src/main/java/com/chenlf/community/controller/DiscussPostController.java
+++ src/main/java/com/chenlf/community/controller/DiscussPostController.java
@@ -3,14 +3,13 @@ package com.chenlf.community.controller;
 import com.chenlf.community.entity.DiscussPost;
 import com.chenlf.community.entity.User;
 import com.chenlf.community.service.DiscussPostService;
+import com.chenlf.community.service.UserService;
 import com.chenlf.community.util.CommunityUtil;
 import com.chenlf.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -28,6 +27,9 @@ public class DiscussPostController {
     private DiscussPostService discussPostService;
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private HostHolder hostHolder;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
@@ -43,7 +45,17 @@ public class DiscussPostController {
         discussPost.setContent(content);
         discussPost.setCreateTime(new Date());
         discussPostService.insertDiscussPost(discussPost);
-
         return CommunityUtil.getJSONString(0,"操作成功");
     }
+
+
+    @RequestMapping(path = "detail/{discussPostId}",method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId")int id, Model model){
+        DiscussPost discussPost = discussPostService.findDiscussPostById(id);
+        model.addAttribute("post",discussPost);
+        User user = userService.findUserById(discussPost.getUserId());
+        model.addAttribute("user",user);
+        return "/site/discuss-detail";
+    }
+
 }
