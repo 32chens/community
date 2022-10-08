0969d3c23147e79999f2867c2a5cd5d7222c4398
chenlf <647854697@qq.com>
2022-10-08 22:48:58 +0800

    redis整合,修改redistemplate泛型并测试

---
+++ src/test/java/com/chenlf/community/RedisTest.java
@@ -0,0 +1,122 @@
+package com.chenlf.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ * @author ChenLF
+ * @date 2022/10/08 20:09
+ **/
+
+@SpringBootTest(classes = {CommunityApplication.class})
+@ContextConfiguration(classes = RedisTest.class)
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testRedsi(){
+        String redisKey = "test:count";
+
+        redisTemplate.opsForValue().set(redisKey,1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+    }
+
+    @Test
+    public void testhash(){
+        String key = "test:user";
+        redisTemplate.opsForHash().put(key,"id", 1);
+        redisTemplate.opsForHash().put(key,"username", "zhangsan");
+
+        System.out.println(redisTemplate.opsForHash().get(key,"id"));
+        System.out.println(redisTemplate.opsForHash().get(key,"username"));
+    }
+
+    @Test
+    public void testList(){
+        String redisKey = "test:ids";
+        redisTemplate.opsForList().leftPush(redisKey,101);
+        redisTemplate.opsForList().leftPush(redisKey,102);
+        redisTemplate.opsForList().leftPush(redisKey,103);
+
+        System.out.println(redisTemplate.opsForList().size(redisKey));
+        System.out.println(redisTemplate.opsForList().index(redisKey,0));
+        System.out.println(redisTemplate.opsForList().range(redisKey,0,2));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+    }
+
+    @Test
+    public void testSets(){
+        String key = "test:teacher";
+        redisTemplate.opsForSet().add(key,"刘备","关羽","张飞");
+        System.out.println(redisTemplate.opsForSet().size(key));
+        System.out.println(redisTemplate.opsForSet().pop(key));
+        System.out.println(redisTemplate.opsForSet().members(key));
+    }
+
+    @Test
+    public void testZset(){
+        String redisKey = "test:student";
+        redisTemplate.opsForZSet().add(redisKey,"唐僧",80);
+        redisTemplate.opsForZSet().add(redisKey,"悟空",90);
+        redisTemplate.opsForZSet().add(redisKey,"八戒",70);
+
+        System.out.println(redisTemplate.opsForZSet().zCard(redisKey));
+        System.out.println(redisTemplate.opsForZSet().score(redisKey,"八戒"));
+        System.out.println(redisTemplate.opsForZSet().rank(redisKey,"八戒"));
+        System.out.println(redisTemplate.opsForZSet().range(redisKey,0,2));
+    }
+
+    @Test
+    public void testKey(){
+        redisTemplate.delete("test:user");
+        System.out.println(redisTemplate.hasKey("test:user"));
+        redisTemplate.expire("test:student",10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testBoundOperations(){
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+    }
+
+    /**
+     * 编程式事务
+     */
+    @Test
+    public void testTransaction(){
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
+
+                operations.multi();     //开启事务
+                operations.opsForSet().add(redisKey, "zhangsan");
+                operations.opsForSet().add(redisKey, "lisi");
+                operations.opsForSet().add(redisKey, "wanwu");
+                System.out.println(operations.opsForSet().members(redisKey));
+                List exec = operations.exec();//提交事务
+                return exec;
+            }
+        });
+
+        System.out.println(obj);
+    }
+
+}
